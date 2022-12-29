b74aa9ef70f1a7ba3342ffdd6e90ecedea3cbfa1
Alex Jackson <alexjackson@macbook-pro.home>
2022-12-29 11:53:56 +0000

    Favicon added

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,27 @@
-import { Routes, Route } from "react-router-dom";
-import './App.css';
-import Intro from './screens/Intro';
-import AboutPage from './screens/AboutPage';
-import ShowsPage from './screens/ShowsPage';
-import MusicPage from './screens/MusicPage';
-import GalleryPage from './screens/GalleryPage';
-import ContactPage from './screens/ContactPage';
+import { Routes, Route } from 'react-router-dom'
+import './App.css'
+import Nav from './components/Nav'
+import Intro from './screens/Intro'
+import AboutPage from './screens/AboutPage'
+import ShowsPage from './screens/ShowsPage'
+import MusicPage from './screens/MusicPage'
+import GalleryPage from './screens/GalleryPage'
+import ContactPage from './screens/ContactPage'
 
-function App() {
+function App () {
   return (
-    <div className="App">
-        <Routes>
-          <Route path="/" element={<Intro />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/shows" element={<ShowsPage />} />
-          <Route path="/releases" element={<MusicPage />} />
-          <Route path="/gallery" element={<GalleryPage />} />
-          <Route path="/contact" element={<ContactPage />} />
-        </Routes>
+    <div className='App'>
+      <Nav />
+      <Routes>
+        <Route path='/' element={<Intro />} />
+        <Route path='/about' element={<AboutPage />} />
+        <Route path='/shows' element={<ShowsPage />} />
+        <Route path='/releases' element={<MusicPage />} />
+        <Route path='/gallery' element={<GalleryPage />} />
+        <Route path='/contact' element={<ContactPage />} />
+      </Routes>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
