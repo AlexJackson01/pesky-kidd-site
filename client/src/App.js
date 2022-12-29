8843c1c8f77a301e9797fa6ab7a187b09b0d8422
Alex Jackson <alexjackson@macbook-pro.home>
2022-12-29 11:44:56 +0000

    Routing and screens added

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import { Routes, Route } from "react-router-dom";
 import './App.css';
+import Intro from './screens/Intro';
+import AboutPage from './screens/AboutPage';
+import ShowsPage from './screens/ShowsPage';
+import MusicPage from './screens/MusicPage';
+import GalleryPage from './screens/GalleryPage';
+import ContactPage from './screens/ContactPage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Routes>
+          <Route path="/" element={<Intro />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/shows" element={<ShowsPage />} />
+          <Route path="/releases" element={<MusicPage />} />
+          <Route path="/gallery" element={<GalleryPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+        </Routes>
     </div>
   );
 }
