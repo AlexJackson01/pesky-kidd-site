b0c6b3bd7da48e931c01863709482b6c80639b6d
AlexJackson01 <ajjackson22@gmail.com>
2023-01-05 10:16:19 +0000

    Muted video so it autoplays

--- client/src/screens/Intro.js
+++ client/src/screens/Intro.js
@@ -4,7 +4,7 @@ import Video from '../assets/videos/intro.mp4'
 export default function Intro () {
   return (
     <div>
-      <video loop autoPlay controls className='intro-video'>
+      <video loop autoPlay muted controls className='intro-video'>
         <source src={Video} type='video/mp4' />
       </video>
     </div>
