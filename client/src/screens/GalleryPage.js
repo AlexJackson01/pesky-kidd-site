2048b51e5949534ad0a8ac31bb727d30ca6ae625
AlexJackson01 <ajjackson22@gmail.com>
2023-01-09 17:19:37 +0000

    Added padding to gallery page

--- client/src/screens/GalleryPage.js
+++ client/src/screens/GalleryPage.js
@@ -9,7 +9,7 @@ export default function GalleryPage() {
   return (
     <div className='app-body container'>
               <h1 className='header'>Gallery</h1>
-              <Grid container spacing={0} sx={{          backgroundColor: 'rgba(68, 78, 90, 0.6)',
+              <Grid container spacing={0} sx={{          backgroundColor: 'rgba(68, 78, 90, 0.6)', paddingBottom: 3
 }}>
                 <Grid item xs={12} sm={12} md={6}>
                   <PhotoCarousel />
