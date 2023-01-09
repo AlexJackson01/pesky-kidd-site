b7c742e2f1ab93b3d7e2ca54819b7be27d24616f
AlexJackson01 <ajjackson22@gmail.com>
2023-01-09 18:01:44 +0000

    Reveal effects added to gallery

--- client/src/screens/GalleryPage.js
+++ client/src/screens/GalleryPage.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { Grid } from '@mui/material'
 import PhotoCarousel from '../components/gallery/photos/PhotoCarousel';
 import VideoCarousel from '../components/gallery/videos/VideoCarousel';
+import { Fade } from 'react-reveal';
 
 export default function GalleryPage() {
 
@@ -9,6 +10,7 @@ export default function GalleryPage() {
   return (
     <div className='app-body container'>
               <h1 className='header'>Gallery</h1>
+              <Fade bottom>
               <Grid container spacing={0} sx={{          backgroundColor: 'rgba(68, 78, 90, 0.6)', paddingBottom: 3
 }}>
                 <Grid item xs={12} sm={12} md={6}>
@@ -18,6 +20,7 @@ export default function GalleryPage() {
                   <VideoCarousel />
                 </Grid>
               </Grid>
+              </Fade>
     </div>
   )
 }
