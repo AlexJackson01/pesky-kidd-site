a6e800c0770a173c1b8b69a61ae3c616e0b269a4
AlexJackson01 <ajjackson22@gmail.com>
2023-03-06 09:53:54 +0000

    About blurb updated

--- client/src/components/about/Blurb.js
+++ client/src/components/about/Blurb.js
@@ -4,19 +4,15 @@ export default function Blurb () {
   return (
     <div className='blurb-div'>
       <p className='band-blurb'>
-        Pesky Kidd is a rock band from London that formed in early
-        2022. Summer last year, they started playing in pubs and clubs around
-        London and by Christmas had amassed a small but loyal following. 2023
-        saw them move up to playing some of the better music venues for amateur
-        bands in London (like the O2 Academy Islington twice, and booking
-        upcoming shows at Camden Assembly, Bobstock and Fiddler's Elbow amongst
-        other dates in March and April). Influenced by bands ranging from System
-        of a Down to Nirvana, Green Day to Radiohead, the band's sound is melody
-        driven, often fusing a traditional writing style and hooky pop-punk and
-        alt rock elements with contemporary production values. The latest
-        track "With You" is the first single from the upcoming 2nd album, which
-        is expected to be released early Summer 2023 and is a great example of
-        what's to come.
+        Pesky Kidd is a rock duo from London that formed in early 2022. Summer
+        last year, they started playing in pubs and clubs around London and by
+        Christmas had amassed a small but loyal following. 2023 saw them move up
+        to playing some of the better music venues for amateur bands in London
+        (like the O2 Academy Islington twice) and supporting Pulled Apart By
+        Horses. Pesky Kidd is influenced by bands ranging from System of a Down
+        to Nirvana, Green Day to Radiohead. The band's sound is melody driven,
+        often fusing a traditional writing style, and hooky pop-punk and alt rock
+        elements with contemporary production values.
       </p>
     </div>
   )
