b3dde7cfaf51969322d1c42505047e38d75bb012
AlexJackson01 <ajjackson22@gmail.com>
2023-03-26 17:49:28 +0100

    Added to band blurb

--- client/src/components/about/Blurb.js
+++ client/src/components/about/Blurb.js
@@ -8,11 +8,24 @@ export default function Blurb () {
         last year, they started playing in pubs and clubs around London and by
         Christmas had amassed a small but loyal following. 2023 saw them move up
         to playing some of the better music venues for amateur bands in London
-        (like the O2 Academy Islington twice) and supporting Pulled Apart By
-        Horses. Pesky Kidd is influenced by bands ranging from System of a Down
-        to Nirvana, Green Day to Radiohead. The band's sound is melody driven,
-        often fusing a traditional writing style, and hooky pop-punk and alt rock
-        elements with contemporary production values.
+        (like the O2 Academy, Islington, Camden Assembly and The Fiddler's
+        Elbow) and supporting Pulled Apart By Horses. The latest release "With
+        You" has already been played on the Voice FM "Punk on the Rocks" show,
+        Black Cat Radio's "New Music and Alternative Classics" with James Page,
+        Surrey Hills Radio's "Transmission", The Island Radio's "Alternative"
+        show, and Black Country Radio.
+      </p>
+      <p className='band-blurb'>
+        Pesky Kidd always supports people in need and in March 2023, they took
+        part in “Rising”, a Charity online show for WarChild UK.
+      </p>
+      <p className='band-blurb'>
+        Pesky Kidd is influenced by bands ranging from System of a Down to
+        Nirvana, Green Day to Radiohead. The band's sound is melody driven,
+        often fusing a traditional writing style, and hooky pop-punk and alt
+        rock elements with contemporary production values. Each track is related
+        to a turbulent or joyful experience and tries to express the emotional
+        reactions to those situations as authentically as possible.
       </p>
     </div>
   )
